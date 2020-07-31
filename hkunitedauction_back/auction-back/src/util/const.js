d26dbc65c522fef020ba50d7d53cd53b4f52abb5
laoking2017 <laoking@live.com>
2020-07-31 21:36:09 +0800

    no message

--- hkunitedauction_back/auction-back/src/util/const.js
+++ hkunitedauction_back/auction-back/src/util/const.js
@@ -1,3 +1,3 @@
 export const FILE_BASEURL = 'http://localhost:3000';
 //export const API_BASEURL = 'http://localhost:8710';
-export const API_BASEURL = 'http://localhost:8888';
+export const API_BASEURL = 'http://console:8888';
