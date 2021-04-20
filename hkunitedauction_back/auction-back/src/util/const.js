33d0378a9e0908b8c1ae10e9176f0f9eed64eaf0
laoking2017 <laoking@live.com>
2021-04-20 08:32:05 +0800

    no message

--- hkunitedauction_back/auction-back/src/util/const.js
+++ hkunitedauction_back/auction-back/src/util/const.js
@@ -1,4 +1,4 @@
-//export const FILE_BASEURL = 'http://localhost:3000';
-export const FILE_BASEURL = '/upload_dir';
-//export const API_BASEURL = 'http://localhost:8710';
-export const API_BASEURL = '/api';
+export const FILE_BASEURL = 'http://localhost:3000/upload_dir';
+//export const FILE_BASEURL = '/upload_dir';
+export const API_BASEURL = 'http://localhost:8888/api';
+//export const API_BASEURL = '/api';
--- hkunitedauction_front/auction-front/src/util/const.js
+++ hkunitedauction_front/auction-front/src/util/const.js
@@ -1,2 +1,2 @@
 export const FILE_BASEURL = 'http://localhost:3000';
-export const API_BASEURL = 'http://localhost:8710';
+export const API_BASEURL = 'http://localhost:8889/api';
